48941cbe1c806daa5edad43f7237ebe00b52daa0
hugosundberg <hugosundberg98@gmail.com>
2024-09-01 17:59:16 +0200

    api started

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-function getWeather() {
+const apiKey = "a7cd134423540c6192a1c60adb560896";
+const cityInput = document.getElementById('location');
 
-    const input = document.getElementById('location').value;
 
-    console.log(input);
+async function getWeatherData() {
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    console.log(response);
+
+    return await response.json();
 
 }
+
+function displayWeatherInfor(data) {
+    const {name: city, 
+            main: {tenp, humidity},
+            weather: [{description, id}]} = data;
+
+    console.log(data)
+}
+
+function getWeatherEmoji(weatherId) {
+
+}
+
+function displayError(mesage) {
+    const errorDisplay = document.createElement("p");
+     
+}
+
+
+
+
